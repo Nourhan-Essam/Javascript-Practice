424f6606fe8696f6259ff9f003d2b6bc1ec5ebfc
Nourhan-Essam <nourhanessam102@gmail.com>
2026-03-17 23:46:22 +0200

    add DOM Demo

---
+++ DOM/dom.js
@@ -0,0 +1,54 @@
+/* DOM Style
+// let body = document.body.style.background = "#FDF5E6";
+// let h1 = document.body.innerHTML = "<h1>Happy Eid!!<h1>";
+// let head = document.getElementById("hi");
+// head.innerHTML = "<h1>Happy Eid!!</h1>";
+// head.style.color = "blue";
+
+// let text =  document.getElementsByClassName("eid")[0];
+// let texts =  document.getElementsByClassName("eid")[1];
+// text.innerHTML = "<h2>Eid Mubarak!!</h2>";
+// text.style.color = "green";
+// texts.style.color = "green";
+
+// let head = document.getElementsByTagName("h1")[0];
+// head.innerHTML = "<h1>Happy Eid!!</h1>";
+// head.style.color = "blue";
+
+// let page = document.querySelector("#hi");
+// page.style.color = "red";
+
+//  let page = document.querySelector(".eid");
+//  page.style.color = "red";
+
+// let page = document.querySelectorAll(".eid");
+// page.style.color = "red";
+
+//  let page = document.querySelector("p");
+//  page.style.color = "red";*/
+
+// let img = document.querySelector("img");
+// console.log(img.alt);
+// console.log(img.hasAttribute('alt'));
+// console.log(img.attributes[1]);
+// console.log(img.setAttribute('alt', 'alt2'));
+// console.log(img.removeAttribute('alt'));
+
+document.body.style.setProperty('background','url("https://img.freepik.com/premium-psd/happy-eid-greetings-islamic-background-islamic-social-media-template_1268394-29.jpg")');
+document.body.style.setProperty('background-repeat', 'no-repeat');
+document.body.style.setProperty('background-size', 'cover');
+// document.body.style.setProperty('background-position', 'center');
+let head = document.createElement("h1");
+head.innerHTML = "<h1>Eid Mubarak!!</h1>";
+head.style.textAlign = "center";
+head.style.color = "#40E0D0";
+head.style.marginTop = "200px";
+head.style.fontFamily = "Italic";
+document.body.appendChild(head);
+
+let page = document.createElement("h3");
+page.innerHTML = "<h3>May your Eid holiday be filled with happiness and joy</h3>"
+page.style.textAlign = "center";
+page.style.color = "#40E0D0";
+page.style.fontFamily = "Italic";
+document.body.appendChild(page);
