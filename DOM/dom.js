c1d2f52eb3f8c90d6c3c0d1252f63303d8792859
Nourhan-Essam <nourhanessam102@gmail.com>
2026-03-19 06:35:54 +0200

    adding Eid Project

--- DOM/dom.js
+++ DOM/dom.js
@@ -34,21 +34,3 @@
 // console.log(img.setAttribute('alt', 'alt2'));
 // console.log(img.removeAttribute('alt'));
 
-document.body.style.setProperty('background','url("https://img.freepik.com/premium-psd/happy-eid-greetings-islamic-background-islamic-social-media-template_1268394-29.jpg")');
-document.body.style.setProperty('background-repeat', 'no-repeat');
-document.body.style.setProperty('background-size', 'cover');
-// document.body.style.setProperty('background-position', 'center');
-let head = document.createElement("h1");
-head.innerHTML = "<h1>Eid Mubarak!!</h1>";
-head.style.textAlign = "center";
-head.style.color = "#40E0D0";
-head.style.marginTop = "200px";
-head.style.fontFamily = "Italic";
-document.body.appendChild(head);
-
-let page = document.createElement("h3");
-page.innerHTML = "<h3>May your Eid holiday be filled with happiness and joy</h3>"
-page.style.textAlign = "center";
-page.style.color = "#40E0D0";
-page.style.fontFamily = "Italic";
-document.body.appendChild(page);
---
+++ Eid Project/Eid.js
@@ -0,0 +1,20 @@
+document.body.style.setProperty('background','url("https://img.freepik.com/premium-psd/happy-eid-greetings-islamic-background-islamic-social-media-template_1268394-29.jpg")');
+document.body.style.setProperty('background-repeat', 'no-repeat');
+document.body.style.setProperty('background-position', 'center');
+document.body.style.setProperty('background-size', 'cover');
+let head = document.createElement("h1");
+head.innerHTML = "<h1>Eid Mubarak!!🎉</h1>";
+head.style.textAlign = "center";
+head.style.color = "#784212";
+head.style.marginTop = "550px";
+head.style.fontFamily = "Italic";
+document.body.appendChild(head);
+
+let page = document.createElement("h3");
+page.innerHTML = "<h3>May your Eid holiday be filled with happiness and joy🤲🎉</h3>"
+page.style.textAlign = "center";
+page.style.color = "#784212";
+head.style.marginTop = "200px";
+head.style.lineHeight = "1.6";
+page.style.fontFamily = "Italic";
+document.body.appendChild(page);
