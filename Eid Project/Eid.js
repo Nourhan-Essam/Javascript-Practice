7bd54e370ceea49694a5576a450d46bfce5a4029
Nourhan-Essam <nourhanessam102@gmail.com>
2026-03-19 07:03:53 +0200

    add some style to Eid Project

--- Eid Project/Eid.js
+++ Eid Project/Eid.js
@@ -6,15 +6,17 @@ let head = document.createElement("h1");
 head.innerHTML = "<h1>Eid Mubarak!!🎉</h1>";
 head.style.textAlign = "center";
 head.style.color = "#784212";
-head.style.marginTop = "550px";
-head.style.fontFamily = "Italic";
+head.style.marginTop = "200px";
+head.style.fontStyle = "Italic";
+head.style.textShadow = "2px 2px 4px rgba(0,0,0,0.3)";
 document.body.appendChild(head);
 
 let page = document.createElement("h3");
 page.innerHTML = "<h3>May your Eid holiday be filled with happiness and joy🤲🎉</h3>"
 page.style.textAlign = "center";
 page.style.color = "#784212";
-head.style.marginTop = "200px";
-head.style.lineHeight = "1.6";
-page.style.fontFamily = "Italic";
+page.style.marginTop = "5px";
+page.style.lineHeight = "1.9";
+page.style.fontStyle = "Italic";
+page.style.textShadow = "2px 2px 4px rgba(0,0,0,0.3)";
 document.body.appendChild(page);
