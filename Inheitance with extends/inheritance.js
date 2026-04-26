e8a65df19f922a3e63b9c9ebf4e542639ab08da8
Nourhan-Essam <nourhanessam102@gmail.com>
2026-04-26 04:27:29 +0300

    adding inheritance

---
+++ Inheitance with extends/inheritance.js
@@ -0,0 +1,24 @@
+class Vehicle{
+    move(){
+        console.log("Vehicle is moving.");
+    }
+}
+class Car extends Vehicle{
+    drive(){
+        console.log("Car is moving.");
+    }
+}
+class Bike extends Vehicle{
+    move(){
+        console.log("Bike is moving.");
+    }
+}
+const car = new Car();
+const bike = new Bike();
+console.log(car.move());                  //Vehicle is moving.
+console.log(car.drive());                //Car is moving.
+console.log(bike.move());               //Bike is moving.
+console.log(car instanceof Vehicle);   //true
+console.log(car instanceof Car);      //true
+console.log(bike instanceof Vehicle);//true
+console.log(bike instanceof Car);   //false
