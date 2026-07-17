5bced18a1c38dcb3e65b6e313f1cf13c349feb5c
Nourhan-Essam <nourhanessam102@gmail.com>
2026-07-18 00:12:36 +0300

    adding promise combinators

--- Promise Basics/promise.js
+++
@@ -1,8 +0,0 @@
-const isAvailable = new Promise((resolve, reject) =>{
-    const isOnline = true;
-    if(isOnline)
-        resolve("Connected");
-    else
-        reject("No Connection");
-});
-isAvailable.then(data => console.log(data)); //Connected
